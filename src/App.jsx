e3afd1e82dca4a790dcda6a7d314fe4fda118254
Dineshsethu001 <dineshsethu15981@gmail.com>
2025-05-25 15:09:27 +0530

    All room finsher

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import { useLocation, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
 function App() {  
   const isOwnerPath = useLocation().pathname.includes("owner")
   return (
@@ -11,6 +13,8 @@ function App() {
       <div className="home min-h-[70vh]">
           <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/rooms" element={<AllRooms/>}/>
+            <Route path="/rooms/:id" element={<RoomDetails/>}/>
           </Routes>
       </div>
           <Footer />
---
+++ src/pages/RoomDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function RoomDetails() {
+  return (
+    <div>
+        <h1 className="pt-64">
+            Room Details
+        </h1>
+    </div>
+  )
+}
+
+export default RoomDetails
